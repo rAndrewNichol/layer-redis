92347eda443bf0335a73ee456b5c78fb4eef229e
jamesbeedy <jamesbeedy@gmail.com>
2016-12-15 22:20:32 +0000

    initial work on redis charm

---
+++ reactive/redis_server.py
@@ -0,0 +1,14 @@
+from charms.reactive import when, when_not, set_state
+from charmhelpers.core.hookenv import unit_private_ip, status_set, config
+
+
+@when_not('redis.configured')
+def config_redis():
+    open_port(config('port'))
+    set_state('redis.port.configured')
+
+@when('redis.available', 'redis.port.configured')
+@when_not('redis.data.set')
+def set_relational_data(redis):
+    redis.provide_data(port=config('port'), password=config('password')
+    set_state('redis.data.set')
