03eb3ae08ee0689534ec2bbaf2cee16dc6fe73f6
jamesbeedy <jamesbeedy@gmail.com>
2019-02-17 16:09:04 -0800

    implement stop hook
    Handling the 'stop' hook was needed in order to remove a node
    from the cluster in an orderly fashion.

--- src/reactive/redis.py
+++ src/reactive/redis.py
@@ -1,3 +1,6 @@
+from time import sleep
+import json
+
 from subprocess import (
     call,
     check_output,
@@ -48,15 +51,51 @@ from charms.layer.redis import (
 )
 
 
+def get_cluster_nodes_info():
+    cluster_nodes = []
+    cmd = '{} -n 0 cluster nodes'.format(REDIS_CLI)
+    out = check_output(cmd, shell=True)
+    for node in out.decode().split('\n')[:-1]:
+        node = node.split()
+        cluster_nodes.append({
+            'node_ip': node[1].split("@")[0].split(":")[0],
+            'node_id': node[0]
+        })
+    return cluster_nodes
+
+
 @when_not('redis.cluster.standalone.determined')
 def set_flag_for_redis_cluster_if_enabled():
+    """Set utility flag for redis-cluster
+    """
+
     if config('cluster-enabled'):
         set_flag('redis.cluster.enabled')
     set_flag('redis.cluster.standalone.determined')
 
 
+@when_not('redis.system.configured')
+def configure_system_for_redis():
+    """Configurations for production redis.
+    (only applied to non-container based machines)
+    """
+
+    if not is_container():
+        with open('/etc/sysctl.conf', 'a') as f:
+            f.write("\nvm.overcommit_memory = 1")
+        call('sysctl vm.overcommit_memory=1'.split())
+
+        with open('/sys/kernel/mm/transparent_hugepage/enabled', 'w') as f:
+            f.write('never')
+
+        with open('/proc/sys/net/core/somaxconn', 'w') as f:
+            f.write('1024')
+
+    set_flag('redis.system.configured')
+
+
 @when('redis.cluster.enabled')
-@when_not('leadership.set.cluster_created')
+@when_not('leadership.set.init_masters')
 @when_any('endpoint.cluster.peer.joined',
           'endpoint.cluster.peer.changed')
 def ensure_sufficient_masters():
@@ -72,35 +111,19 @@ def ensure_sufficient_masters():
         status.blocked('No peer endpoint set')
         return
 
+    # Get the peers, check for min length
     peers = endpoint_from_flag(endpoint).all_units
-    peer_ips = ["{}:6379".format(peer._data['private-address'])
+    peer_ips = [peer._data['private-address']
                 for peer in peers if peer._data is not None]
-
     if len(peer_ips) > 1:
         status.active(
             "Minimum # masters available, got {}.".format(len(peer_ips)+1))
         init_masters = \
-            " ".join(peer_ips) + " {}:6379".format(unit_private_ip())
+            ",".join(peer_ips + [unit_private_ip()])
         charms.leadership.leader_set(init_masters=init_masters)
 
     clear_flag('endpoint.cluster.peer.joined')
     clear_flag('endpoint.cluster.peer.changed')
-
-
-@when_not('redis.system.configured')
-def configure_system_for_redis():
-    if not is_container():
-        with open('/etc/sysctl.conf', 'a') as f:
-            f.write("\nvm.overcommit_memory = 1")
-        call('sysctl vm.overcommit_memory=1'.split())
-
-        with open('/sys/kernel/mm/transparent_hugepage/enabled', 'w') as f:
-            f.write('never')
-
-        with open('/proc/sys/net/core/somaxconn', 'w') as f:
-            f.write('1024')
-
-    set_flag('redis.system.configured')
 
 
 @when('snap.installed.redis-bdx',
@@ -108,12 +131,17 @@ def configure_system_for_redis():
       'redis.cluster.standalone.determined')
 @when_not('redis.system.pre-init.complete')
 def set_pre_init():
+    """Set utility pre-init flag
+    """
     set_flag('redis.system.pre-init.complete')
 
 
 @when('redis.system.pre-init.complete')
 @when_not('redis.ready')
 def write_config_start_restart_redis():
+    """Write config, restart service
+    """
+
     ctxt = {'port': config('port'),
             'databases': config('databases'),
             'log_level': config('log-level'),
@@ -162,9 +190,46 @@ def set_redis_version():
 
 
 @when('redis.ready',
+      'leadership.is_leader',
+      'redis.cluster.enabled',
+      'redis.port.accessible',
+      'leadership.set.init_masters')
+@when_not('redis.cluster.created',
+          'leadership.set.cluster_created')
+def create_redis_cluster():
+    """Create redis cluster
+    """
+
+    status.maintenance("creating cluster...")
+
+    init_masters = charms.leadership.leader_get("init_masters")
+    init_masters_split = init_masters.split(",")
+    init_masters_str = \
+        " ".join(["{}:6379".format(ip) for ip in init_masters_split])
+
+    cmd = ('{} --cluster create {} --cluster-replicas 0').format(
+        REDIS_CLI, init_masters_str)
+    p = Popen(cmd.split(), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    std_out = p.communicate(input='yes\n'.encode())[0]
+
+    log(std_out)
+
+    charms.leadership.leader_set(cluster_node_ips=init_masters)
+    charms.leadership.leader_set(cluster_created="true")
+    charms.leadership.leader_set(
+        cluster_nodes_json=json.dumps(
+            get_cluster_nodes_info()))
+
+    set_flag('redis.cluster.joined')
+    set_flag('redis.cluster.created')
+
+
+@when('redis.ready',
       'redis.cluster.enabled')
 @when_not('leadership.set.init_masters')
 def waiting_for_min_masters():
+    """Waiting for min masters
+    """
     status.blocked("Need at least 3 master nodes to bootstrap cluster...")
     return
 
@@ -182,36 +247,10 @@ def are_we_in_status():
         set_flag('redis.cluster.joined')
 
 
-@when('redis.ready',
-      'leadership.is_leader',
-      'redis.cluster.enabled',
-      'redis.port.accessible',
-      'leadership.set.init_masters')
-@when_not('redis.cluster.created',
-          'leadership.set.cluster_created')
-def create_redis_cluster():
-    """Create redis cluster
-    """
-
-    status.maintenance("creating cluster...")
-
-    init_masters = charms.leadership.leader_get("init_masters")
-
-    cmd = ('{} --cluster create {} --cluster-replicas 0').format(
-            REDIS_CLI, init_masters)
-    p = Popen(cmd.split(), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-    std_out = p.communicate(input='yes\n'.encode())[0]
-
-    log(std_out)
-
-    charms.leadership.leader_set(cluster_created="true")
-
-    set_flag('redis.cluster.joined')
-    set_flag('redis.cluster.created')
-
-
 @when('redis.cluster.joined')
 def cluster_joined_status():
+    """Set cluster joined status
+    """
     status.active("successfully clustered")
 
 
@@ -234,19 +273,13 @@ def add_new_peer_nodes_to_cluster():
         status.blocked('No peer endpoint set')
         return
 
-    # Get the known cluster peer ip addressese from juju perspective
+    # Get the known application peer ip addressese from juju perspective
     peers = endpoint_from_flag(endpoint).all_units
     peer_ips = [peer._data['private-address']
                 for peer in peers if peer._data is not None]
 
     # Get the known cluster node ips from redis point of view
-    cluster_node_ips = []
-    cmd = '{} -n 0 cluster nodes'.format(REDIS_CLI)
-    out = check_output(cmd, shell=True)
-    log(out)
-    for node in out.decode().split('\n')[:-1]:
-        node = node.split()
-        cluster_node_ips.append(node[1].split("@")[0].split(":")[0])
+    cluster_node_ips = [node['node_ip'] for node in get_cluster_nodes_info()]
 
     # Compare the nodes in the cluster to the peer nodes that juju is aware of
     # Register nodes that are juju peers, but not part of the cluster
@@ -260,22 +293,18 @@ def add_new_peer_nodes_to_cluster():
             log(out)
 
     # Give the cluster a second to recognize the new node
-    from time import sleep
     sleep(1)
 
     if node_added:
-        # Get the known cluster node ids (including the recently added node)
-        cluster_node_ids = []
-        cluster_node_ips = []
-        cmd = '{} -n 0 cluster nodes'.format(REDIS_CLI)
-        out = check_output(cmd, shell=True)
-        log(out)
-        for node in out.decode().split('\n')[:-1]:
-            node = node.split()
-            cluster_node_ids.append(node[0])
-            cluster_node_ips.append(node[1].split("@")[0].split(":")[0])
-  
-        charms.leadership.leader_set(cluster_node_ips=",".join(cluster_node_ips))
+        cluster_nodes = get_cluster_nodes_info()
+        cluster_node_ips = [node['node_ip'] for node in cluster_nodes]
+        cluster_node_ids = [node['node_id'] for node in cluster_nodes]
+
+        charms.leadership.leader_set(
+            cluster_node_ips=",".join(cluster_node_ips))
+        charms.leadership.leader_set(
+            cluster_nodes_json=json.dumps(cluster_nodes))
+
         # Generate the weights string for the rebalance command
         node_weights = " ".join(["{}=1".format(node_id)
                                  for node_id in cluster_node_ids])
@@ -291,7 +320,10 @@ def add_new_peer_nodes_to_cluster():
 
 # Client Relation
 @when('endpoint.redis.joined')
-def provide_client_relation_data():
+def provide_client_endpoint_data():
+    """Send data over endpoint
+    """
+
     endpoint = endpoint_from_flag('endpoint.redis.joined')
     ctxt = {'host': unit_private_ip(), 'port': config('port')}
     if config('password'):
@@ -303,6 +335,9 @@ def provide_client_relation_data():
 @when('nrpe-external-master.available')
 @when_not('redis.nagios-setup.complete')
 def setup_nagios(nagios):
+    """Setup nagios check
+    """
+
     conf = config()
     unit_name = local_unit()
     check_base = '/usr/lib/nagios/plugins/'
@@ -323,3 +358,27 @@ def setup_nagios(nagios):
 @hook('config-changed')
 def set_nrpe_flag():
     clear_flag('redis.nagios-setup.complete')
+
+
+@hook('stop')
+def rebalance_and_remove():
+    """Rebalance and remove.
+    Rebalance the node slots before removal.
+    """
+    if is_flag_set('redis.cluster.joined'):
+        nodes_info_json = charms.leadership.leader_get("cluster_nodes_json")
+        nodes_info = json.loads(nodes_info_json)
+        for node in nodes_info:
+            if node['node_ip'] == unit_private_ip():
+                # Rebalance slots away from node to remove
+                cmd = ("{} --cluster rebalance {}:6379 "
+                       "--cluster-weight {}=0").format(
+                           REDIS_CLI, node['node_ip'], node['node_id'])
+                out = check_output(cmd, shell=True)
+                log(out)
+                sleep(1)
+                # Remove node from cluster
+                cmd = "{} --cluster del-node {}:6379 {}".format(
+                    REDIS_CLI, node['node_ip'], node['node_id'])
+                out = check_output(cmd, shell=True)
+                log(out)
